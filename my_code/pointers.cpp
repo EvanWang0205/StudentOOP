822b978cb6cc802d138c08a1e5128b7c09ffafac
EvanWang0205 <yw4086@nyu.edu>
2019-06-04 12:52:33 -0400

    111

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -1,36 +1,86 @@
 #include <iostream>
+
 #include "pointers.h"
+
+
 
 using namespace std;
 
+
+
 void inc_by_value(int n) {
+	++n;
 }
+
+
+
 
 
 void inc_with_pointer(int* iptr) {
+	++(&ipter);
 }
+
+
+
 
 
 void inc_with_reference(int& n) {
+	++n;
 }
+
+
+
 
 
 Thing** create_array_of_things(int n) {
-    return nullptr;
+	Thing** array = new Thing*[n]
+		for (int i = 0, i <= n, i++) {
+			array[i] = new thing(i);
+	}
+	return array;
+
 }
+
+
 
 void print_all_things(Thing** things, int n) {
+	for (int i = 0, i <= n, i++) {
+		cout << things[i] << " ";
+	}
 }
+
+
 
 void double_all_things(Thing** things, int n) {
+	for (int i = 0, i <= n, i++) {
+		things[i]->val *= 2;
+	}
 }
 
+
+
 void delete_all_things(Thing** things, int n) {
+	for (int i = 1, i <= n, i++) {
+		delete things[i];
+	}
+	delete[] things
 }
+
 
 
 void assignTA(Student& s, Student& ta) {
+    s -> ta = ta
 }
 
+
 void printTAs(vector<Student> students) {
+    for (Student* student : student){
+        if(student-> ta == nullptr){
+            cout << student -> name << "has no TA\n";
+        }
+        else{
+            cout << student -> name << "has  TA" << student -> ta -> name << endl;
+        }
+    }
+
 }
