c19b58ecfac5257f67a506d06b21903c946bd234
EvanWang0205 <yw4086@nyu.edu>
2019-06-07 13:02:17 -0400

    update

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -4,22 +4,46 @@
 
 using namespace std;
 
-/*
- * A constructor for weather class.
- * */
-Weather::Weather(std::string nm, GPS loc) :
-station_nm(nm), my_loc(loc) {
+Date::Date(int d, int m, int y){
+    if((d < 1) || (d > 31)) throw (d);
+    if((m < 1) || (m > 12)) throw (m);
+    if((y < 1800) || (y > 2200)) throw (y);
+    day = d;
+    month = m;
+    year = y;
 }
-ostream& operator << (ostream& os,const Weather& w){
-    int r = w.get_rating();
-    os << w.station_nm << ": rating" << r <<
-    "longitude" << w.my_loc.longitude <<
-    "latitude" << w.my_loc.latitude << endl;
+
+ostream& operator<<(ostream& os, const Date& date){
+    os << date.month << "/" << date.day << "/" << date.year;
     return os;
 }
 
 
-string Weather:: get_name() const{
+ostream& operator<<(ostream& os, const WReading& wr){
+    os << "Date: " << wr.date << "; Humidity: " << wr.humidity << "; Temperature: " << wr.temperature << "; Windspeed: " << wr.windspeed;
+    return os;
+}
+
+
+/*
+ * Output GPS
+ **/
+
+ostream& operator<<(ostream& os, const GPS& gps){
+    os << "latitude : " << gps.latitude << "; longitude: " << gps.longitude;
+    return os;
+}
+
+/*
+ * A constructor for weather class.
+ * */
+Weather::Weather(string nm, GPS loc, int rating) :
+station_nm(nm), my_loc(loc), rating(rating) {
+}
+
+
+
+string Weather::get_name() const {
     return station_nm;
 }
 
@@ -31,9 +55,17 @@ void Weather::set_rating(int new_rating){
     rating = new_rating;
 }
 
-//string Weather::get_name() const{
-//    return name;
-//ostream& operator<<(ostream&  os, const Weather& w){
-//    os<<w.station_nm;
-//    return os;
+void Weather::add_reading(WReading wr){
+    wreadings.push_back(wr);
+}
+
+
+ostream& operator<<(ostream& os, const Weather& w){
+    os << w.get_name() << "rating : " << w.get_rating() << "; location: "
+    << w.my_loc;
+    for(WReading wr : w.wreadings){
+        os << "   " << wr << endl;
+    }
+    return os;
+}
 
