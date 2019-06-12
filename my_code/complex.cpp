dc3fecc828f1f5a277dd8b7208fde8faada876d1
EvanWang0205 <yw4086@nyu.edu>
2019-06-12 15:02:51 -0400

    111

--- my_code/complex.cpp
+++ my_code/complex.cpp
@@ -5,11 +5,17 @@
 
 using namespace std;
 
+//Complex::Complex(double real, double imag) :  real(real), imag(imag) {}
+
 bool operator== (const Complex& c1, const Complex& c2) {
-    return true;  // temp!
+    if ((c1.get_real()) - (c2.get_real() <= 0.001) && (c1.get_imag()) - (c2.get_imag() <= 0.001))
+        return true;
+    return false;  // temp!
 }
 
 ostream& operator<< (ostream& os, const Complex& c) {
+    os << "Real:" << setprecision(3) << c.real << "Imaginary:" << showpos << c.imag << noshowpos;
+    return os;
     /*
      * Outputting a `Complex` instance, while illustrating some of the
      * capabilities of I/O streams: `setprecision` gives us a fixed
@@ -18,31 +24,77 @@ ostream& operator<< (ostream& os, const Complex& c) {
      * */
 }
 
+Complex::Complex(double real, double imag) : real(real), imag(imag) {}
+
 
 /*
  * Read a `Complex` number from an input stream.
  * */
 istream& operator>> (istream& is, Complex& c) {
+    is >> c.real >>c.imag;
+    return is;
 }
 
 
 Complex::operator bool() const {
+    if (real == 0 && imag == 0)
+        return false;
+    return true;
 }
 
 Complex& Complex::operator++() {
+    ++real;
+    return *this;
 }
 
 Complex Complex::operator++(int dummy) {
-}
-
-Complex Complex::operator+(const Complex& c) {
+    Complex temp = *this;
+    real++;
+    return temp;
 }
 
 double Complex::get_real() const {
+    return real;
 }
 
 double Complex::get_imag() const {
+    return imag;
 }
 
 Complex Complex::operator*(const int i) {
+    imag *= i;
+    real *= i;
+    return *this;
+}
+
+Complex Complex::operator-=(const Complex& c){
+    real -= c.real;
+    imag -= c.imag;
+    return *this;
+    //return Complex(real, imag)
+}
+
+Complex& Complex::operator--(){
+    --real;
+    return *this;
+}
+
+Complex Complex::operator--(int dummy){
+    Complex temp = *this;
+    real--;
+    return temp;
+}
+
+Complex operator+(const Complex& c1, const Complex& c2){
+    double temp_real = c1.get_real() + c2.get_real();
+    double temp_imag = c1.get_imag() + c2.get_imag();
+    Complex temp = (temp_real, temp_imag);
+    return temp;
+}
+
+Complex operator-(const Complex& c1, const Complex& c2){
+    double temp_real = c1.get_real() - c2.get_real();
+    double temp_imag = c1.get_imag() - c2.get_imag();
+    Complex temp = (temp_real, temp_imag);
+    return temp;
 }
--- my_code/weather.h
+++ my_code/weather.h
@@ -14,9 +14,9 @@ struct GPS {
 std::ostream& operator<<(std::ostream& os, const GPS& gps);
 
 class Date{
-    friend std::ostream& operator<<(std::ostream& os, const Date& date);
+friend std::ostream& operator<<(std::ostream& os, const Date& date);
 public:
-    Date(int d, int m, int y);
+    Date(int d, int m, int );
 private:
     int day;
     int month;
@@ -24,10 +24,14 @@ private:
 };
 
 class WReading{
-    friend std::ostream& operator<<(std::ostream& os, const WReading& wr);
+friend std::ostream& operator<<(std::ostream& os, const WReading& wr);
 public:
     WReading(Date dt, double temp, double hum, double ws):
-    date(dt), temperature(temp),humidity(hum),windspeed(ws){}
+        date(dt), temperature(temp),humidity(hum),windspeed(ws){}
+    
+    double get_tempF();
+    double get_tempC(){return temperature;};
+
 private:
     Date date;
     double temperature;
@@ -41,8 +45,8 @@ const int OK = 1;
 const int GOOD = 2;
 
 class Weather {
-    // << should output name, rating, and use the GPS << to output my_loc
-    
+// << should output name, rating, and use the GPS << to output my_loc
+
     friend std::ostream& operator<<(std::ostream& os, const Weather& w);
 public:
     Weather(std::string nm, GPS loc, int rating);
@@ -50,6 +54,8 @@ public:
     int get_rating() const;
     void set_rating(int new_rating);
     void add_reading(WReading wr);
+    int get_heat_index();
+    int get_wind_chill();
 private:
     std::vector<WReading> wreadings;
     std::string station_nm;
@@ -57,5 +63,35 @@ private:
     int rating = UNRATED;
 };
 
+class Image {
+public:
+    Image(int w, int h, std::string flnm);
+    // copy constructor:
+    Image(const Image& img2);
+    ~Image();
+    Image& operator=(const Image& img2);
+    int image_sz();
+    
+    /*
+     * Setting `display() = 0` here makes this an abstract
+     * class that can't be implemented.
+     * */
+    std::string display(std::string s);
+    /*
+     * If we don't want virtual method lookup, we
+     * could just declare:
+     * void display();
+     * */
+    
+    int get_height() { return height; }
+    int get_width() { return width; }
+    
+private:
+    int width;
+    int height;
+    std::string filename;
+    char* image_buf;
+    void copy_fields(const Image& img2);
+};
 
 #endif
